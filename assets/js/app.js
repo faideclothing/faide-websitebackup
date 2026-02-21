59066fd876bc5582f38a559e7b4c5cc6d3524a8c
faideclothing <faideclothingsa@gmail.com>
2026-02-21 21:53:27 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,9 @@
-<>
+document.addEventListener("DOMContentLoaded", function () {
+
+  const menuBtn = document.getElementById("menuBtn");
+
+  menuBtn.addEventListener("click", function () {
+    alert("Menu coming soon 🔥");
+  });
+
+});
